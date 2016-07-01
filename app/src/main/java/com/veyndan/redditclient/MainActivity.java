169fe9df60283f46b2fca2f0fe514faab93194c7
veyndan <veyndan@gmail.com>
2016-07-01 15:16:59 +0100

    Set up Reddit wrapper library

--- app/src/main/java/com/veyndan/redditclient/MainActivity.java
+++ app/src/main/java/com/veyndan/redditclient/MainActivity.java
@@ -1,7 +1,10 @@
 package com.veyndan.redditclient;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import rawjava.Reddit;
+import rawjava.network.Credentials;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Credentials credentials = Credentials.create(getResources().openRawResource(R.raw.credentials));
+        Reddit reddit = new Reddit(credentials);
     }
 }
