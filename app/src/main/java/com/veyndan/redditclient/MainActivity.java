481fb29d03c59fb23916098c5d0ac964565354be
veyndan <veyndan@gmail.com>
2016-07-11 18:08:56 +0100

    Add pagination

--- app/src/main/java/com/veyndan/redditclient/MainActivity.java
+++ app/src/main/java/com/veyndan/redditclient/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 
+import com.jakewharton.rxbinding.support.v7.widget.RxRecyclerView;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,15 +14,22 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import rawjava.Reddit;
 import rawjava.model.Link;
+import rawjava.model.Listing;
 import rawjava.model.Thing;
 import rawjava.network.Credentials;
 import rawjava.network.Sort;
+import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends BaseActivity {
 
     private static final String TAG = "veyndan_MainActivity";
+
+    private LinearLayoutManager layoutManager;
+
+    private boolean loadingPosts;
+    private int pastVisiblesItems, visibleItemCount, totalItemCount;
 
     @BindView(R.id.recycler_view) RecyclerView recyclerView;
 
@@ -35,19 +44,34 @@ public class MainActivity extends BaseActivity {
 
         final List<Thing<Link>> posts = new ArrayList<>();
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
         recyclerView.addItemDecoration(new DividerItemDecoration(this));
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         final PostAdapter postAdapter = new PostAdapter(posts, reddit, metrics.widthPixels);
         recyclerView.setAdapter(postAdapter);
-
-        reddit.subreddit("all", Sort.HOT)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
+        reddit.subreddit("all", Sort.HOT, getNextPageTrigger(), Schedulers.io(), AndroidSchedulers.mainThread())
                 .subscribe(post -> {
                     posts.addAll(post.data.children);
                     postAdapter.notifyDataSetChanged();
+                    loadingPosts = false;
+                });
+    }
+
+    Observable<Thing<Listing<Thing<Link>>>> getNextPageTrigger() {
+        return RxRecyclerView.scrollEvents(recyclerView)
+                .filter(scrollEvent -> scrollEvent.dy() > 0) //check for scroll down
+                .map(scrollEvent -> {
+                    visibleItemCount = layoutManager.getChildCount();
+                    totalItemCount = layoutManager.getItemCount();
+                    pastVisiblesItems = layoutManager.findFirstVisibleItemPosition();
+                    return scrollEvent;
+                })
+                .filter(scrollEvent -> !loadingPosts && visibleItemCount + pastVisiblesItems >= totalItemCount)
+                .map(scrollEvent -> {
+                    loadingPosts = true;
+                    return null;
                 });
     }
 }
