33c1af251d23d97aa14dca046820b27eb39f13b5
veyndan <veyndan@gmail.com>
2016-08-24 15:27:35 +0100

    Replace Preconditions.checkNonNull() from Guava with Java sdks Objects.requireNonNull()

--- app/src/main/java/com/veyndan/redditclient/api/reddit/network/interceptor/UserAgentInterceptor.java
+++ app/src/main/java/com/veyndan/redditclient/api/reddit/network/interceptor/UserAgentInterceptor.java
@@ -1,8 +1,7 @@
 package com.veyndan.redditclient.api.reddit.network.interceptor;
 
-import com.google.common.base.Preconditions;
-
 import java.io.IOException;
+import java.util.Objects;
 
 import okhttp3.Interceptor;
 import okhttp3.Request;
@@ -13,7 +12,7 @@ public final class UserAgentInterceptor implements Interceptor {
     private final String userAgentHeaderValue;
 
     public UserAgentInterceptor(final String userAgentHeaderValue) {
-        this.userAgentHeaderValue = Preconditions.checkNotNull(userAgentHeaderValue);
+        this.userAgentHeaderValue = Objects.requireNonNull(userAgentHeaderValue);
     }
 
     @Override
