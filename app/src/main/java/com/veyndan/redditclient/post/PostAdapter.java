e1811e803cb3660fa2a3ce2f8785768e5f9b43b5
veyndan <veyndan@gmail.com>
2016-09-01 14:04:46 +0100

    Extend RecyclerView adapters with ListDelegationAdapter

--- app/src/main/java/com/veyndan/redditclient/post/PostAdapter.java
+++ app/src/main/java/com/veyndan/redditclient/post/PostAdapter.java
@@ -1,10 +1,8 @@
 package com.veyndan.redditclient.post;
 
 import android.app.Activity;
-import android.support.v7.widget.RecyclerView;
-import android.view.ViewGroup;
 
-import com.hannesdorfmann.adapterdelegates2.AdapterDelegatesManager;
+import com.hannesdorfmann.adapterdelegates2.ListDelegationAdapter;
 import com.veyndan.redditclient.api.reddit.Reddit;
 import com.veyndan.redditclient.post.delegate.PostAdapterDelegate;
 import com.veyndan.redditclient.post.delegate.ProgressAdapterDelegate;
@@ -12,39 +10,14 @@ import com.veyndan.redditclient.post.model.Post;
 
 import java.util.List;
 
-public class PostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class PostAdapter extends ListDelegationAdapter<List<Post>> {
 
-    private final AdapterDelegatesManager<List<Post>> delegatesManager;
-
-    private final List<Post> posts;
-
-    public PostAdapter(final Activity activity, final List<Post> posts, final Reddit reddit, final int width) {
-        this.posts = posts;
-
-        delegatesManager = new AdapterDelegatesManager<>();
-
+    public PostAdapter(final Activity activity, final List<Post> posts, final Reddit reddit,
+                       final int width) {
         delegatesManager
                 .addDelegate(new PostAdapterDelegate(this, activity, reddit, width))
                 .addDelegate(new ProgressAdapterDelegate());
-    }
 
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
-        return delegatesManager.onCreateViewHolder(parent, viewType);
-    }
-
-    @Override
-    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
-        delegatesManager.onBindViewHolder(posts, position, holder);
-    }
-
-    @Override
-    public int getItemCount() {
-        return posts.size();
-    }
-
-    @Override
-    public int getItemViewType(final int position) {
-        return delegatesManager.getItemViewType(posts, position);
+        setItems(posts);
     }
 }
--- app/src/main/java/com/veyndan/redditclient/post/media/PostMediaAdapter.java
+++ app/src/main/java/com/veyndan/redditclient/post/media/PostMediaAdapter.java
@@ -3,10 +3,8 @@ package com.veyndan.redditclient.post.media;
 import android.app.Activity;
 import android.support.customtabs.CustomTabsClient;
 import android.support.customtabs.CustomTabsIntent;
-import android.support.v7.widget.RecyclerView;
-import android.view.ViewGroup;
 
-import com.hannesdorfmann.adapterdelegates2.AdapterDelegatesManager;
+import com.hannesdorfmann.adapterdelegates2.ListDelegationAdapter;
 import com.veyndan.redditclient.post.media.delegate.ImageAdapterDelegate;
 import com.veyndan.redditclient.post.media.delegate.LinkAdapterDelegate;
 import com.veyndan.redditclient.post.media.delegate.LinkImageAdapterDelegate;
@@ -16,45 +14,18 @@ import com.veyndan.redditclient.post.model.Post;
 
 import java.util.List;
 
-public class PostMediaAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-
-    private final AdapterDelegatesManager<List<Object>> delegatesManager;
-
-    private final List<Object> items;
+public class PostMediaAdapter extends ListDelegationAdapter<List<Object>> {
 
     public PostMediaAdapter(final Activity activity, final CustomTabsClient customTabsClient,
                             final CustomTabsIntent customTabsIntent, final Post post,
                             final int width, final List<Object> items) {
-        this.items = items;
-
-        delegatesManager = new AdapterDelegatesManager<>();
-
-        // AdapterDelegatesManager internally assigns view types integers
         delegatesManager
                 .addDelegate(new TextAdapterDelegate())
                 .addDelegate(new LinkAdapterDelegate(activity, customTabsClient, customTabsIntent, post))
                 .addDelegate(new LinkImageAdapterDelegate(activity, customTabsClient, customTabsIntent, post))
                 .addDelegate(new ImageAdapterDelegate(activity, customTabsClient, customTabsIntent, post, width))
                 .addDelegate(new TweetAdapterDelegate());
-    }
 
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
-        return delegatesManager.onCreateViewHolder(parent, viewType);
-    }
-
-    @Override
-    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
-        delegatesManager.onBindViewHolder(items, position, holder);
-    }
-
-    @Override
-    public int getItemCount() {
-        return items.size();
-    }
-
-    @Override
-    public int getItemViewType(final int position) {
-        return delegatesManager.getItemViewType(items, position);
+        setItems(items);
     }
 }
