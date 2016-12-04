7ae5edc86ecc44b9138a186c99d3559b805abf50
veyndan <veyndan@gmail.com>
2016-12-04 14:34:06 +0000

    Make Imgure API wrapper not return null in any case

--- app/src/main/java/com/veyndan/paper/reddit/api/imgur/model/Album.java
+++ app/src/main/java/com/veyndan/paper/reddit/api/imgur/model/Album.java
@@ -1,7 +1,16 @@
 package com.veyndan.paper.reddit.api.imgur.model;
 
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Album {
-    public List<Image> images;
+
+    @NonNull private final List<Image> images = new ArrayList<>();
+
+    @NonNull
+    public List<Image> getImages() {
+        return images;
+    }
 }
--- app/src/main/java/com/veyndan/paper/reddit/api/imgur/network/ImgurService.java
+++ app/src/main/java/com/veyndan/paper/reddit/api/imgur/network/ImgurService.java
@@ -1,5 +1,7 @@
 package com.veyndan.paper.reddit.api.imgur.network;
 
+import android.support.annotation.NonNull;
+
 import com.veyndan.paper.reddit.api.imgur.model.Album;
 import com.veyndan.paper.reddit.api.imgur.model.Basic;
 
@@ -9,6 +11,7 @@ import retrofit2.http.Path;
 
 public interface ImgurService {
 
+    @NonNull
     @GET("album/{id}")
-    Single<Basic<Album>> album(@Path("id") String id);
+    Single<Basic<Album>> album(@NonNull @Path("id") String id);
 }
