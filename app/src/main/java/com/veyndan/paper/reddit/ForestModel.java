5462993dface04f1a91903f0fb7cafb5066a849a
veyndan <veyndan@gmail.com>
2017-01-13 15:15:05 +0000

    Define ForestModel

--- app/src/main/java/com/veyndan/paper/reddit/ForestModel.java
+++ app/src/main/java/com/veyndan/paper/reddit/ForestModel.java
@@ -1,7 +1,52 @@
 package com.veyndan.paper.reddit;
 
-public class ForestModel {
-    // TODO Define the fields required to classify the state of the whole Reddit Feed. This means
-    //      that the forest field which is a List<Node<T>> will contain all the elements that
-    //      the RecyclerView is displaying.
+import com.hannesdorfmann.adaptercommands.command.AdapterCommand;
+import com.veyndan.paper.reddit.util.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class ForestModel<T> implements Model {
+
+    private final List<Node<T>> trees;
+    private final List<AdapterCommand> adapterCommands;
+
+    private ForestModel(final Builder<T> builder) {
+        trees = builder.trees;
+        adapterCommands = builder.adapterCommands;
+    }
+
+    public List<Node<T>> getTrees() {
+        return trees;
+    }
+
+    public static class Builder<T> {
+
+        private final List<Node<T>> trees = new ArrayList<>();
+        private final List<AdapterCommand> adapterCommands = new ArrayList<>();
+
+        public Builder<T> tree(final Node<T> tree) {
+            trees.add(tree);
+            return this;
+        }
+
+        public Builder<T> trees(final List<Node<T>> trees) {
+            this.trees.addAll(trees);
+            return this;
+        }
+
+        public Builder<T> adapterCommand(final AdapterCommand adapterCommand) {
+            adapterCommands.add(adapterCommand);
+            return this;
+        }
+
+        public Builder<T> adapterCommands(final List<AdapterCommand> adapterCommands) {
+            this.adapterCommands.addAll(adapterCommands);
+            return this;
+        }
+
+        public ForestModel<T> build() {
+            return new ForestModel<>(this);
+        }
+    }
 }
---
+++ app/src/main/java/com/veyndan/paper/reddit/Model.java
@@ -0,0 +1,4 @@
+package com.veyndan.paper.reddit;
+
+public interface Model {
+}
