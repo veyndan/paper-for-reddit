3ad77e1fe902b1a9c1077a5c4091b0707ea85be7
veyndan <veyndan@gmail.com>
2017-02-12 15:47:18 +0000

    Remove problematic imports

--- app/src/main/java/com/veyndan/paper/reddit/deeplink/CustomDeepLinkHandler.java
+++ app/src/main/java/com/veyndan/paper/reddit/deeplink/CustomDeepLinkHandler.java
@@ -5,8 +5,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.airbnb.deeplinkdispatch.DeepLinkHandler;
-import com.veyndan.paper.reddit.AppDeepLinkModuleLoader;
-import com.veyndan.paper.reddit.DeepLinkDelegate;
 
 @DeepLinkHandler(AppDeepLinkModule.class)
 public class CustomDeepLinkHandler extends AppCompatActivity {
