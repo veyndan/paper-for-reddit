76ecd739e0573ee7405c036e5bd3aac06ee77c4d
veyndan <veyndan@gmail.com>
2017-04-15 14:52:16 +0100

    Make the Downstream of the transformer the UiModel

--- app/src/main/java/com/veyndan/paper/reddit/NextPageUiModel.java
+++ app/src/main/java/com/veyndan/paper/reddit/NextPageUiModel.java
@@ -1,4 +1,31 @@
 package com.veyndan.paper.reddit;
 
-public class NextPageUiModel {
+import com.veyndan.paper.reddit.api.reddit.model.Listing;
+import com.veyndan.paper.reddit.api.reddit.model.Thing;
+import com.veyndan.paper.reddit.util.Node;
+
+import java.util.Collections;
+import java.util.List;
+
+import retrofit2.Response;
+
+public final class NextPageUiModel {
+
+    public static NextPageUiModel node(final Node<Response<Thing<Listing>>> node) {
+        return new NextPageUiModel(Collections.singletonList(node));
+    }
+
+    public static NextPageUiModel nodes(final List<Node<Response<Thing<Listing>>>> nodes) {
+        return new NextPageUiModel(nodes);
+    }
+
+    private final List<Node<Response<Thing<Listing>>>> nodes;
+
+    private NextPageUiModel(final List<Node<Response<Thing<Listing>>>> nodes) {
+        this.nodes = nodes;
+    }
+
+    public List<Node<Response<Thing<Listing>>>> getNodes() {
+        return nodes;
+    }
 }
