28505dcebc7e0685f5f2cef0452d7884cff6aea5
veyndan <veyndan@gmail.com>
2017-04-15 14:42:14 +0100

    Move loadNode logic to transformer

--- app/src/main/java/com/veyndan/paper/reddit/NextPageEvent.java
+++ app/src/main/java/com/veyndan/paper/reddit/NextPageEvent.java
@@ -1,4 +1,20 @@
 package com.veyndan.paper.reddit;
 
+import com.veyndan.paper.reddit.api.reddit.model.Listing;
+import com.veyndan.paper.reddit.api.reddit.model.Thing;
+import com.veyndan.paper.reddit.util.Node;
+
+import retrofit2.Response;
+
 public final class NextPageEvent {
+
+    private final Node<Response<Thing<Listing>>> node;
+
+    public NextPageEvent(final Node<Response<Thing<Listing>>> node) {
+        this.node = node;
+    }
+
+    public Node<Response<Thing<Listing>>> getNode() {
+        return node;
+    }
 }
