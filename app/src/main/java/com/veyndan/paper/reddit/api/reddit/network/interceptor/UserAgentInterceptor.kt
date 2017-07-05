d49c234e029b5b1b48d5b141f4c95909169e0c69
veyndan <veyndan@gmail.com>
2017-07-05 07:35:09 +0100

    Use Kotlin nullability type system

--- app/src/main/java/com/veyndan/paper/reddit/api/reddit/network/interceptor/UserAgentInterceptor.kt
+++ app/src/main/java/com/veyndan/paper/reddit/api/reddit/network/interceptor/UserAgentInterceptor.kt
@@ -4,14 +4,12 @@ import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 
-class UserAgentInterceptor(userAgentHeaderValue: String) : Interceptor {
+class UserAgentInterceptor(private val userAgentHeaderValue: String) : Interceptor {
 
     companion object {
 
         private const val USER_AGENT_HEADER_NAME = "User-Agent"
     }
-
-    private val userAgentHeaderValue: String = checkNotNull(userAgentHeaderValue)
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest: Request = chain.request()
